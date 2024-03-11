1a9ee03f83a1da4c58cd0c0c0cede77f31d34784
ahmedsaleh210 <ahmed.saleh212020@gmail.com>
2024-03-12 01:14:44 +0200

    update readme and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TestApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
 
   // This widget is the root of your application.
   @override
